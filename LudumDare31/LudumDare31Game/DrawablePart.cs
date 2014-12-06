249c7a67eda298e8ea0ec690018e7d8be30ac2b5
Mindar <alexander.schramm33@gmail.com>
2014-12-06 16:48:40 +0100

    now with extra drawableparts

---
+++ LudumDare31/LudumDare31Game/DrawablePart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LudumDare31Game
+{
+    public class DrawablePart
+    {
+        public void Draw(Game g, int deltatime, TileType tile)
+        {
+            //WorldSetting ws = g.Gamemap.WorldSetting
+        }
+    }
+}
