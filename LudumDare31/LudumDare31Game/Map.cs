2fc6940a2bfe691d9da44d547fe250e99d322a95
Mindar <alexander.schramm33@gmail.com>
2014-12-06 15:19:42 +0100

    added basic map stuff

---
+++ LudumDare31/LudumDare31Game/Map.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LudumDare31Game
+{
+    public class Map
+    {
+        public Tile[][] Tiles { get; set; }
+        public List<Entity> Entities { get; set; }
+    }
+}
---
+++ LudumDare31/LudumDare31Game/Tile.cs
@@ -0,0 +1,13 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LudumDare31Game
+{
+    public class Tile
+    {
+        public Image[] Sprites { get; set; }
+    }
+}
